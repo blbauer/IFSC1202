6ffea8f2377c39e8df664ff1b8318be518632cc7
Bruce Bauer <blbauer@ualr.edu>
2024-01-24 16:39:06 +0000

    Unit 2

---
+++ 02.00.05 Int Round.py
@@ -0,0 +1,11 @@
+print(int(1.3))		# gives 1
+print(int(1.7))		# gives 1
+print(int(-1.3))	# gives -1
+print(int(-1.7))	# gives -1
+
+print(round(1.3))	# gives 1
+print(round(1.7))	# gives 2
+print(round(-1.3))	# gives -1
+print(round(-1.7))	# gives -2
+
+print(round(12.455,2))
---
+++ 02.01 Last Digit.py
@@ -0,0 +1,4 @@
+x = int(input("Enter a number: "))
+y = x % 10
+print(y)
+
