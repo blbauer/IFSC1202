c8b2f972b7d87ea22e0793e6537f0e8c04d3ec09
Bruce Bauer <blbauer@ualr.edu>
2025-01-27 15:25:03 +0000

    Update

--- 01.Project-B.py
+++ 01.Project-B.py
@@ -1,19 +1,19 @@
 # Assume that it is not a leap year
 NumSeconds = int(input("Enter Length of Time in Seconds: "))
 
-SecondsPerYear = 365 * 24 * 60 * 60
+SecondsPerYear = 365 * 24 * 60 * 60     # 31,536,000
 Years = NumSeconds // SecondsPerYear
 NumSeconds = NumSeconds - (Years * SecondsPerYear)
 
-SecondsPerDay = 24 * 60 * 60
+SecondsPerDay = 24 * 60 * 60            # 86,400
 Days  = NumSeconds // SecondsPerDay
 NumSeconds = NumSeconds - (Days * SecondsPerDay)
 
-SecondsPerHour = 60 * 60
+SecondsPerHour = 60 * 60                # 3,600
 Hours  = NumSeconds // SecondsPerHour
 NumSeconds = NumSeconds - (Hours * SecondsPerHour)
 
-SecondsPerMinute = 60
+SecondsPerMinute = 60                   # 60
 Minutes  = NumSeconds // SecondsPerMinute
 NumSeconds = NumSeconds - (Minutes * SecondsPerMinute)
 
