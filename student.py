789d28fb6e614c39a029f69abf4eb3965780de5d
Bruce Bauer <blbauer@ualr.edu>
2025-12-02 15:31:50 +0000

    Update

--- student.py
+++ student.py
@@ -1,169 +1,71 @@
-import os
+"""
+* Create a python solution that reads the US Constitution, prompts for a search term, then displays the relevant sections.
 
-class Student:
+* The text of the US Constitution can be found at https://brucebauer.info/constitution.txt. Click on the link, select all of the text, and paste it into your Codespace with a name of "constitution.txt".
 
-    def __init__ (self, firstname, lastname, tnumber, score=None):
-        
-        self.FirstName = firstname
-        self.LastName = lastname
-        self.TNumber = tnumber
-        self.Grades = score if score is not None else [] 
-        self.TotalAssignments = 0 
 
-#######################################
+* Logic
+    - Open the file constitution.txt and read each line into a list. Be sure to strip out the linefeed character.
+    - Prompt for a search term (exit the program when the search term is blank).
+    - Search for the search term in each line. When the search term is found:
+        Loop backwards from the current line to find the beginning of the section. Look for a blank line.
+        Loop forwards from the current lint to find the end of the section. Look for a blank line.
+        Print the section of text that contains the serach term along with the line number.
+    - Skip to the end of the section. Don't print the section twice if the search term is found twice.
+    - Prompt for another search term.
 
-    def RunningAverages(self):
-        Scores = []
-        for score in self.Grades:
-            if score.strip():
-                Scores.append(int(score))
-        
-        if not Scores:
-            return 0.0
-        
-        return sum(Scores) / len(Scores)
-    
-#######################################
+# Example
+Enter search term: liquor
+Line 273: 
+Line 274: 18th Amendment
+Line 275: Section 1
+Line 276: After one year from the ratification of this article the manufacture, sale, or transportation of intoxicating liquors within, the importation thereof into, or the exportation thereof from the United States and all territory subject to the jurisdiction thereof for beverage purposes is hereby prohibited.
+Line 277: 
+ 
+Line 311: 
+Line 312: Section 2
+Line 313: The transportation or importation into any State, Territory, or possession of the United States for delivery or use therein of intoxicating liquors, in violation of the laws thereof, is hereby prohibited.
+Line 314: 
+ 
+Enter search term:
+"""
 
-    def TotalAverage(self):
-        num_assignments = self.TotalAssignments
-        
-        if num_assignments == 0:
-            return 0.0
+print ("\nConstitution Search Program\n")
 
-        total_score = 0
-        for score in self.Grades:
-            if score.strip():
-                total_score += int(score)
+constitution = []
+const_file = open("constitution.txt", "r")
+line = const_file.readline()
 
-        return total_score / num_assignments 
-    
-#######################################
+while line != "":
+    line = line.rstrip("\n")
+    constitution.append(line)
+    line = const_file.readline()
 
-    def LetterGrade(self):
-        average = self.TotalAverage()
+const_file.close()
 
-        if average >= 90:
-            return "A"
-        elif average >= 80:
-            return "B"
-        elif average >= 70:
-            return "C"
-        elif average >= 60:
-            return "D"
+search = input("Enter search term: ")
+
+while search != "":
+    index = 0
+    while index < len(constitution):
+        current_line = constitution[index]
+        if search.lower() in current_line.lower():
+            top = index
+            while top > 0 and constitution[top] != "":
+                top = top - 1
+            bottom = index
+            while bottom < len(constitution) - 1 and constitution[bottom] != "":
+                bottom = bottom + 1
+            for i in range(top, bottom + 1):
+                if constitution[i] == "":
+                    print ("Line " + str(i + 1) + ":")
+                else:
+                    print ("Line " + str(i + 1) + ":", constitution[i])
+            print ()
+            index = bottom + 1
         else:
-            return "F"
-        
-####################################################3####3
-#Second MAin
-class StudentList: 
-    
-    def __init__ (self):
-        self.Studentlist = []
-        self.MaxAssignments = 0 
+            index = index + 1
 
-#######################################
+    print ()
 
-    def add_student(self, firstname, lastname, tnumber, score=None):
-        
-        newstudent = Student(firstname, lastname, tnumber, score) 
-        self.Studentlist.append(newstudent)
-
-#######################################
-
-    def find_student(self, tnumber):
-
-        for index, student in enumerate(self.Studentlist): 
-            if student.TNumber == tnumber:
-                return index
-        return -1
-
-#######################################
-
-    def print_student_list(self):
-
-        HEADER = (
-            f"\n{'First':>12}{'Last':>12}{'ID':>12}{'Running':>12}{'Semester':>12}{'Letter':>12}\n"
-            f"{'Name':>12}{'Name':>12}{'Number':>12}{'Average':>12}{'Average':>12}{'Grade':>12}\n"
-            f"{'-'*12}{' '}{'-'*11}{' '}{'-'*11}{' '}{'-'*11}{' '}{'-'*11}{' '}{'-'*11}" 
-        )
-        print(HEADER)
-
-        for student in self.Studentlist:
-            student.TotalAssignments = self.MaxAssignments
-
-        for student in self.Studentlist:
-            running_avg = student.RunningAverages() 
-            total_avg = student.TotalAverage()
-            letter_grade = student.LetterGrade()
-
-            output = (
-                        f"{student.FirstName:>12}"
-                        f"{student.LastName:>12}"
-                        f"{student.TNumber:>12}"
-                        f"{running_avg:>12.2f}"
-                        f"{total_avg:>12.2f}"
-                        f"{letter_grade:>12}"
-                        )
-            print(output)
-
-#######################################
-
-    def add_student_from_file(self, filename):
-        with open(filename, 'r') as openfile:
-            for line in openfile:
-                if not line.strip():
-                    continue
-
-                parts = line.strip().split(',')
-
-                if len(parts) >= 3:
-                    first_name, last_name, t_number = [p.strip() for p in parts[:3]]
-                    self.add_student(first_name, last_name, t_number, []) 
-
-#######################################
-    
-    def add_scores_from_file(self, filename):
-        temp_scores_tracker = {student.TNumber: [] for student in self.Studentlist}
-        
-        with open(filename, 'r') as openfile:
-            for line in openfile:
-                if not line.strip():
-                    continue
-
-                parts = line.strip().split(',')
-                
-                if len(parts) >= 2: 
-                    t_number = parts[0].strip() 
-                    score = parts[1].strip()
-
-                    if t_number in temp_scores_tracker:
-                        temp_scores_tracker[t_number].append(score)
-
-        if not temp_scores_tracker:
-            return
-
-        self.MaxAssignments = max(len(scores) for scores in temp_scores_tracker.values())
-        
-        for student in self.Studentlist:
-            t_number = student.TNumber
-            if t_number in temp_scores_tracker:
-                student.Grades = temp_scores_tracker[t_number]
-            
-            student.TotalAssignments = self.MaxAssignments
-
-#######################################1
-
-STUDENTFILE = "11.Project Students.txt"
-SCOREFILE = "11.Project Scores.txt"
-
-def main():
-
-    student_list_manager = StudentList()
-    student_list_manager.add_student_from_file(STUDENTFILE)
-    student_list_manager.add_scores_from_file(SCOREFILE)
-    student_list_manager.print_student_list()
-
-if __name__ == "__main__":
-    main()
-    
+    search = input("Enter search term: ")
